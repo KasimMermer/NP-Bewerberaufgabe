8635c6442b26d2aa4e71ddfea415ae10d2de10c9
Kasim <mermer@advasco.com>
2025-12-18 15:42:21 +0100

    Feature: checkout.js hinzugefügt und aufgerufen (getestet)

--- includes/assets.php
+++ includes/assets.php
@@ -5,3 +5,9 @@ defined('ABSPATH') || exit;
 /**
  * Enqueue der CSS- und JS-Dateien des Plugins.
  */
+add_action( 'wp_enqueue_scripts', 'prefix_load_scripts' );
+ 
+function prefix_load_scripts() {
+    wp_register_script( 'script', plugins_url( '../assets/js/checkout.js', __FILE__ ), array(), '1.0.0', true );
+    wp_enqueue_script( 'script' );
+}
