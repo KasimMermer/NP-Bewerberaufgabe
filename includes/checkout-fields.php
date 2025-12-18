04e40507d6eb3de868906293c297d4a75997f0cd
Kasim <mermer@advasco.com>
2025-12-18 14:08:48 +0100

    Feature: Neues Textfeld hinzugefügt (Geschenkkartentext)

--- includes/checkout-fields.php
+++ includes/checkout-fields.php
@@ -2,9 +2,35 @@
 
 defined('ABSPATH') || exit;
 
-add_action('woocommerce_after_order_notes', 'test_hook');
-
 // Hook funktionierte mit WooCommerce Checkout Seite nicht. So habe ich für den Test einen Shortcode verwendet: [woocommerce_checkout]
+
+/* Verwendete Dokumentation: https://developer.woocommerce.com/docs/code-snippets/customising-checkout-fields
+für "woocommerce_form_field"
+https://woocommerce.github.io/code-reference/hooks/hooks.html
+für "woocommerce_after_order_notes" */
+
+add_action('woocommerce_after_order_notes', 'mermer_add_gift_card_text_area_field');
+
+function mermer_add_gift_card_text_area_field($checkout) {
+    woocommerce_form_field('mermer_gift_card_text_field', array(
+        'type' => 'textarea',
+        'label' => 'Geschenkkartentext',
+        'placeholder' => 'Geben Sie hier Ihren Geschenkkartentext ein',
+        'required' => false,
+        'custom_attributes' => array(
+            'maxlength' => '500',
+        ),
+    ), $checkout->get_value('mermer_gift_card_text_field'));
+}
+
+
+
+
+
+
+
+
+add_action('woocommerce_after_order_notes', 'test_hook');
 
 function test_hook($checkout) {
     echo '<div style="background:red;color:white;display:flex;justify-content:center;">Hook klappt auch</div>';
