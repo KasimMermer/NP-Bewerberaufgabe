60be45b751f2a85311f7371b4bc291eba45507ca
Kasim <mermer@advasco.com>
2025-12-18 15:43:30 +0100

    Feature: 0 - 500 Zeichen eingestellt - es wird unten rechts vom Textarea angezeigt

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,1 +1,21 @@
 alert("hallo test");
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textAreaField = document.getElementById('mermer_gift_card_text_field');
+    
+    const maxLength = textAreaField.getAttribute('maxlength');
+
+    const charCountDisplay = document.createElement('div');
+    charCountDisplay.id = 'char-count-display';
+    charCountDisplay.style.fontSize = '12px';
+    charCountDisplay.style.marginTop = '5px';
+    charCountDisplay.align = 'right';
+    charCountDisplay.textContent = `0 / ${maxLength}`;
+
+    textAreaField.parentNode.appendChild(charCountDisplay);
+
+    textAreaField.addEventListener('input', function() {
+        const currentLength = textAreaField.value.length;
+        charCountDisplay.textContent = `${currentLength} / ${maxLength}`;
+    });
+});
