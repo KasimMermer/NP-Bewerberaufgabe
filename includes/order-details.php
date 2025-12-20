b597b698a604ee0eb4e23ea4414ba6e7163b8d60
Kasim <mermer@advasco.com>
2025-12-20 19:51:49 +0100

    Fix: Gebühren sollen beim Admin und Kunden angezeigt werden

--- includes/order-details.php
+++ includes/order-details.php
@@ -4,10 +4,10 @@ defined('ABSPATH') || exit;
 
 /* 
 Youtube Tutorial für "woocommerce_admin_order_data_after_billing_address" gekommen: https://www.youtube.com/watch?v=jGOYhWKH_Vk 
-ChatGPT löste mir den Fehler, dass es nicht angezeigt wird. Es lag an dem Hook (der nur für Admin-Bestelldetails ist) und gab mir 
-folgenden Vorschlag: "woocommerce_order_details_after_customer_details"
+ChatGPT gab mir die Lösung für die Anzeige beim Kunden: "woocommerce_order_details_after_customer_details"
 */
 
+add_action('woocommerce_admin_order_data_after_billing_address', 'mermer_show_gift_card_text_area_and_gift_wrapping_checkbox_field', 10, 1);
 add_action('woocommerce_order_details_after_customer_details', 'mermer_show_gift_card_text_area_and_gift_wrapping_checkbox_field', 10, 1);
 
 function mermer_show_gift_card_text_area_and_gift_wrapping_checkbox_field($order){
